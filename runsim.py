50c8328afe4cf6845b1249e8573eaedad1219872
Owen McBrien <owenmcbrien@owens-mbp.home>
2021-11-10 16:34:00 +0000

    Moved a few files around so running is cleaner

---
+++ runsim.py
@@ -0,0 +1,189 @@
+from imsu import common_tools, db_queries, population_generator, lc_generator, recovery, plotting_suite
+import shutil
+import sys
+import os
+import json
+import pandas as pd
+
+# =============================
+
+def main():
+
+	(all_settings,
+	program_settings,
+	query_settings,
+	lightcurve_settings,
+	grb_settings,
+	recovery_settings,
+	io_settings) = common_tools.readSettings()
+	
+	common_tools.setPlottingParameters()
+	
+	output_path = os.path.join('results', io_settings['results directory'])
+	
+	if not os.path.exists('results'):
+		os.mkdir('results')
+		
+	if not os.path.exists(output_path):
+		os.mkdir(output_path)
+	
+	shutil.copy('settings.yaml', os.path.join(output_path, 'settings.yaml'))
+	
+	# Query database or read previous query from file
+	
+	if not os.path.exists('query_files'):
+		os.mkdir('query_files')
+	
+	query_path = os.path.join('query_files', query_settings['query file']) + '.csv'
+	
+	if program_settings['Query Database']:
+
+		db_conn = db_queries.establishConnection()
+		query_output = db_queries.queryDB(db_conn, query_settings)
+		query_output.to_csv(query_path, index = False)
+
+	else:
+	
+		if not os.path.exists(query_path):
+			sys.exit('No query results exist. Enable query execution or provide history of pointings.')
+		else:
+			query_output = pd.read_csv(query_path)
+	
+	
+	# Set special treatment for GRB simulations
+	if lightcurve_settings['population']['transient type'] == 'GRB':
+		is_grb = True
+	else:
+		is_grb = False
+	
+	# Establish Survey() class for given transient type
+	if is_grb:
+		the_survey = common_tools.EllipseSurvey(query_output, lightcurve_settings['telescope properties']['column headings'], lightcurve_settings['ellipse geometry'])
+	else:
+		the_survey = common_tools.BoxSurvey(query_output, lightcurve_settings['telescope properties']['column headings'])
+
+	# Begin generating transient population (RA, Dec, z, etc.)	
+	population_path = os.path.join(output_path, lightcurve_settings['population']['population file']) + '.csv'
+	
+	if program_settings['Generate Transients']:
+	
+		if is_grb:
+			
+			population_settings = population_generator.generateGRBPopulation(the_survey, lightcurve_settings)
+		
+			population_settings = pd.DataFrame(population_settings)
+			population_settings.to_csv(population_path, index = False)			
+		
+		else:
+		
+			population_settings = population_generator.generatePopulation(the_survey, lightcurve_settings)
+		
+			population_settings = pd.DataFrame(population_settings)
+			population_settings.to_csv(population_path, index = False)
+
+	else:
+
+		if not os.path.exists(population_path):
+			sys.exit('No population file exists. Enable population generator.')
+		else:
+			population_settings = pd.read_csv(population_path)
+
+	# Generate lightcurves from population
+	lightcurve_path = os.path.join(output_path, lightcurve_settings['lightcurve file']) + '.json'
+	
+	if program_settings['Generate Lightcurves']:
+		
+	
+		if lightcurve_settings['population']['transient type'] == 'GRB':
+			
+			lc_population = lc_generator.generateGRBLightcurvePopulation(query_output, population_settings, lightcurve_settings, grb_settings, io_settings)
+			
+			with open(lightcurve_path, 'w') as f:
+				json.dump(lc_population, f, indent = 4)
+		
+		elif lightcurve_settings['population']['transient type'] == 'Kilonova':
+			
+			source = lc_generator.addTimeSeriesSource(obj_name = 'at2017gfo', explosion_epoch = 2457983.48, redshift = 0.00984, min_wl = 3400., max_wl = 22300., npoints = 40000)
+			lc_population = lc_generator.generateSNLightcurvePopulation(query_output, population_settings, lightcurve_settings, io_settings, source)
+			
+			lc_generator.saveLightcurveData(lightcurve_path, lc_population)
+
+		elif lightcurve_settings['population']['transient type'] == 'FBOT':
+			
+			source = lc_generator.addTimeSeriesSource(obj_name = 'at2018cow', explosion_epoch = 2458284.8, redshift = 0.014145, min_wl = 3400., max_wl = 12000, npoints = 10000)
+			lc_population = lc_generator.generateSNLightcurvePopulation(query_output, population_settings, lightcurve_settings, io_settings, source)
+			
+			lc_generator.saveLightcurveData(lightcurve_path, lc_population)
+
+		elif lightcurve_settings['population']['transient type'] == 'WDNS':
+			
+			source = lc_generator.addTimeSeriesSource(obj_name = 'at2018kzr', explosion_epoch = 2458477.922, redshift = 0.05298, min_wl = 3400., max_wl = 12000, npoints = 10000)
+			lc_population = lc_generator.generateSNLightcurvePopulation(query_output, population_settings, lightcurve_settings, io_settings, source)
+			
+			lc_generator.saveLightcurveData(lightcurve_path, lc_population)
+		
+		else:
+
+			lc_population = lc_generator.generateSNLightcurvePopulation(query_output, population_settings, lightcurve_settings, io_settings, source = None)
+			
+			lc_generator.saveLightcurveData(lightcurve_path, lc_population)
+	
+	else:
+	
+		if not os.path.exists(lightcurve_path):
+			sys.exit('No lightcurve population has been generated. Enable lightcurve generator.')
+		else:
+			with open(lightcurve_path, 'r') as f:
+				lc_population = json.load(f)
+
+	# Run recovery tests
+	recovery_lc_path = os.path.join(output_path, lightcurve_settings['lightcurve file']) + '_rec.json'
+	recovery_pop_path = os.path.join(output_path, lightcurve_settings['lightcurve file']) + '_rec.csv'
+	
+	
+	if program_settings['Perform Recovery']:
+	
+		lc_population = recovery.initialiseRecoveryProcedure(lc_population)
+			
+		lc_population = recovery.performMinimumDetectionCount(lc_population, recovery_settings)
+		lc_population = recovery.performMinimumNightlyDetectionCount(lc_population, recovery_settings)
+			
+		population_settings = recovery.markRecoveriesByFilter(lc_population, recovery_settings, population_settings)
+		print(population_settings)
+		
+		recovery.calculateEfficiencies(recovery_settings, population_settings)
+			
+		recovery.saveRecoveryData(lc_population, population_settings, recovery_lc_path, recovery_pop_path)
+		
+	else:
+		
+		if not os.path.exists(recovery_lc_path):
+			sys.exit('Lightcurve recovery has not been performed. Enable recovery module.')
+		else:
+			with open(recovery_lc_path) as f:
+				lc_population = json.load(f)
+			
+			population_settings = pd.read_csv(recovery_pop_path)
+	
+	if program_settings['Make Plots']:
+		
+		common_tools.setPlottingParameters()
+		
+# 		plotting_suite.make2DHistogramGOTOPointings(query_output, lightcurve_settings)
+# 		plotting_suite.plotLimitingMagnitudesFromQuery(query_output, lightcurve_settings)
+		
+		plotting_suite.makeRedshiftHistogram(population_settings, lightcurve_settings)
+		plotting_suite.makeExplosionEpochHistogram(population_settings, the_survey, lightcurve_settings)
+		
+		plotting_suite.make2DHistogramRecoveredPointings(population_settings, lightcurve_settings)
+
+# 		plotting_suite.plotTransientLightcurves(lc_population, lightcurve_settings)
+# 		plotting_suite.plotRecoveredLightcurves(lc_population, lightcurve_settings, population_settings)
+		
+		
+	
+
+
+if __name__ == '__main__':
+	main()
+	
